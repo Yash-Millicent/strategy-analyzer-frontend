deedc2a38317ce02e6359774efe50526f282e582
Yash-Millicent <146835819+Yash-Millicent@users.noreply.github.com>
2025-02-15 14:07:58 +0530

    Add files via upload
    first commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // base: "/metronic8/react/demo1/",
+  base: "",
+
+  build: {
+    chunkSizeWarningLimit: 3000,
+  },
+})
